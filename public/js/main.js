9e088fdeef00233deac4e962905f5b36fac4e08a
Peter Enriquez <twinjoker54@gmail.com>
2017-05-15 15:44:25 -0600

    Message Component
    Message component Practice

--- public/js/main.js
+++ public/js/main.js
@@ -17,6 +17,28 @@ Vue.component('task', {
 	template: '<li><slot></slot></li>'
 });
 
+Vue.component('message', {
+	
+	props: ['title', 'body'],
+	
+	data() {
+		return {
+			isVisible: true
+		};
+	},
+	
+	template: `
+			<article class="message" v-show="isVisible">
+				<div class="message-header">
+					<p>{{ title }}</p>
+					<button class="delete" @click="isVisible = false"></button>
+				</div>
+				<div class="message-body">
+					{{ body }}
+				</div>
+			</article>`,
+});
+
 new Vue({
 	el: '#root'
 });
